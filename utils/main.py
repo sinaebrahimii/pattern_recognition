23f9d18187a9a0872e83191b9c2164622f20f594
sina ebrahimi <sinaebrahimi31@gmail.com>
2024-11-29 18:41:21 +0330

    t4 added to the files

--- utils/main.py
+++ utils/main.py
@@ -1,6 +1,8 @@
 import  cv2
 from cv2 import typing
 import numpy as np
+from scipy.ndimage import  convolve
+
 
 def make_blocks(img:typing.MatLike,)->list[typing.MatLike]:
     img=cv2.resize(img,(100,100))
@@ -105,4 +107,47 @@ def lbp_calculated_pixel(img, x, y):
           
     return val 
    
- 
+def lbp(image):
+    height, width, _ = image.shape 
+    img_gray = cv2.cvtColor(image, 
+                        cv2.COLOR_BGR2GRAY) 
+    img_lbp = np.zeros((height, width), 
+                   np.uint8)
+    for i in range(0, height): 
+        for j in range(0, width): 
+            img_lbp[i, j] = lbp_calculated_pixel(img_gray, i, j)
+    return img_lbp
+
+
+def lpq(image, win_size=3):
+    
+    STFTalpha = 1 / win_size
+    conv_mode = 'reflect'
+    
+    x = np.arange(-(win_size - 1) // 2, (win_size - 1) // 2 + 1, dtype=float)
+    n = len(x)
+    w0 = np.ones_like(x) / np.sqrt(n)
+
+    w1 = np.exp(-2 * np.pi * 1j * x * STFTalpha)
+    
+    f = np.zeros((n, n), dtype=complex)
+
+    f[:, 0] = w0
+    f[:, 1] = w1
+    f[:, 2] = np.conj(w1)
+    
+    img = image.astype('float32')
+    f_img = np.zeros((3, img.shape[0], img.shape[1]), dtype=complex)
+
+    for i in range(3):
+        f_img[i] = convolve(img, np.real(f[:, i].reshape(n, 1)), mode=conv_mode) + \
+                   1j * convolve(img, np.imag(f[:, i].reshape(n, 1)), mode=conv_mode)
+
+    # Create LPQ descriptor by thresholding the real and imaginary parts
+    lpq_desc = ((np.angle(f_img[0]) > 0).astype(int) +
+                2 * (np.angle(f_img[1]) > 0).astype(int) +
+                4 * (np.angle(f_img[2]) > 0).astype(int)).astype(np.uint8)
+    
+    return lpq_desc
+
+
